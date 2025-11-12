7b17dc841cf629b4c9a4c2104c13cf5e8b4ee635
Agathamudi vinay kumar <146352277+vinaythanay@users.noreply.github.com>
2025-11-12 14:48:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import pickle
 import time
-from langchain import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
